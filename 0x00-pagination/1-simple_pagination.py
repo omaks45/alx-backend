efe8e5c7927fa64a89bc824a23bd95ea3af3a493
Okoro Omaka <107546452+omaks45@users.noreply.github.com>
2023-05-18 07:26:08 +0100

    1-simple_pagination.py

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,27 +1,17 @@
 #!/usr/bin/env python3
+"""Simple pagination
+    Copy index_range from the previous task
 """
-Write a function named index_range that takes two integer \
-        arguments page and page_size.
-The function should return a tuple of size two containing a start
-index and an end index corresponding to the range of indexes to return
-in a list for those particular pagination parameters.
-Page numbers are 1-indexed, i.e. the first page is page 1.
-"""
-
-
-import typing
 import csv
-from typing import List
-import math
+from typing import List, Tuple
 
 
-def index_range(page: int, page_size: int) -> typing.Tuple[int, int]:
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Page numbers are 1-indexed, the first page is page 1.
     """
-    returns in a list for those particular pagination parameters.
-    """
-    # page - 1 is the current page
-    # page_size is the page size to divide the data_set into
-    return ((page - 1) * page_size, page * page_size)
+    start = (page - 1) * page_size
+    end = start + page_size
+    return (start, end)
 
 
 class Server:
@@ -30,6 +20,8 @@ class Server:
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
+        """Initializes a new Server instance.
+        """
         self.__dataset = None
 
     def dataset(self) -> List[List]:
@@ -44,11 +36,12 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """return the appropriate page of the dataset """
+        """Retrieves a page of data.
+        """
         assert type(page) == int and type(page_size) == int
         assert page > 0 and page_size > 0
         start, end = index_range(page, page_size)
         data = self.dataset()
         if start > len(data):
             return []
-            return data[start:end]
+        return data[start:end]
