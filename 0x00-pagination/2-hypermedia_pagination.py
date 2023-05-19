51199ca8ccb2d9017515cb184bcd914d8c8901dd
Okoro Omaka <107546452+omaks45@users.noreply.github.com>
2023-05-19 09:37:09 +0100

    2-hypermedia_pagination.py

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -1,17 +1,37 @@
 #!/usr/bin/env python3
-"""Hypermedia pagination
+"""Replicate code from the previous task.
+
+Implement a get_hyper method that takes the same arguments
+(and defaults) as get_page and returns a dictionary containing
+the following key-value pairs:
+
+page_size: the length of the returned dataset page
+page: the current page number
+data: the dataset page (equivalent to return from previous task)
+next_page: number of the next page, None if no next page
+prev_page: number of the previous page, None if no previous page
+total_pages: the total number of pages in the dataset as an integer
+Make sure to reuse get_page in your implementation.
+
+You can use the math module if necessary.
 """
+
+
+from typing import Tuple, List
 import csv
 import math
-from typing import Dict, List, Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """Page numbers are 1-indexed, the first page is page 1.
     """
-    start = (page - 1) * page_size
-    end = start + page_size
-    return (start, end)
+    start index and an end index corresponding to the range of
+    """
+    # if page is 1, start at 0 and end at page_size
+    # if page is 2, start at ((page-1) * page_size) and
+    # end at (page_size * page)
+    # if page is 3, start at ((page-1) * page_size) and
+    # end at (page_size * page)
+    return ((page-1) * page_size, page_size * page)
 
 
 class Server:
@@ -20,8 +40,6 @@ class Server:
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
-        """Initializes a new Server instance.
-        """
         self.__dataset = None
 
     def dataset(self) -> List[List]:
@@ -36,28 +54,49 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """Retrieves a page of data.
-        """
-        assert type(page) == int and type(page_size) == int
-        assert page > 0 and page_size > 0
-        start, end = index_range(page, page_size)
-        data = self.dataset()
-        if start > len(data):
-            return []
-        return data[start:end]
+        """return the appropriate page of the dataset"""
+        assert type(page) is int and page > 0
+        assert type(page_size) is int and page_size > 0
 
-    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
-        """Retrieves information about a page.
+        # get the data from the csv
+        data = self.dataset()
+
+        try:
+            # get the index to start and end at
+            start, end = index_range(page, page_size)
+            return data[start:end]
+        except IndexError:
+            return []
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
+        """returns a dictionary containing the following key-value pairs
         """
-        page_index = self.get_page(page, page_size)
+        assert type(page) is int and page > 0
+        assert type(page_size) is int and page_size > 0
+
+        page_data = self.get_page(page, page_size)
+        total_pages = math.ceil(len(self.dataset()) / page_size)
+
         start, end = index_range(page, page_size)
-        total_pages = math.ceil(len(self.__dataset) / page_size)
-        page_info = {
-            'page_size': len(page_index),
+
+        # checking for next page if exist
+        if (page < total_pages):
+            next_page = page+1
+        else:
+            next_page = None
+
+        # checking if the current page exists
+        if (page == 1):
+            prev_page = None
+        else:
+            prev_page = page - 1
+
+        page_detail =  {
+            'page_size': len(page_data),
             'page': page,
-            'data': page_data,
-            'next_page': page + 1 if end < len(self.__dataset) else None,
-            'prev_page': page - 1 if start > 0 else None,
-            'total_pages': total_pages,
+            'data': data,
+            'next_page': next_page,
+            'prev_page': prev_page,
+            'total_pages': total_pages
         }
-        return page_info
+        return page_detail
